85ed4cd39629245cdff54eb9acae6f8aae46ee6d
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-02-28 14:18:44 +0000

    Add bare-bone Collision Editor

--- OpenKh.Tools.Common/Utilities.cs
+++ OpenKh.Tools.Common/Utilities.cs
@@ -1,15 +1,23 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+using System.Windows;
 
 namespace OpenKh.Tools.Common
 {
     public static class Utilities
     {
+        public static Window GetCurrentWindow() =>
+            Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
+
         public static string GetApplicationName()
         {
             var assembly = Assembly.GetEntryAssembly();
             var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
             return fvi.ProductName;
         }
+
+        public static void ShowError(string message, string title = "Error") =>
+            MessageBox.Show(GetCurrentWindow(), message, title, MessageBoxButton.OK, MessageBoxImage.Error);
     }
 }
